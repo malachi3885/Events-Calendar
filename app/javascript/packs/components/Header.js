1d71f9e021a0360fee31d823e7bffaeebfdf2e6e
malachi3885 <malachi3885@gmail.com>
2021-07-29 12:40:15 +0700

    Change color add button to white.

--- app/javascript/packs/components/Header.js
+++ app/javascript/packs/components/Header.js
@@ -6,7 +6,7 @@ const Header = (props) => {
   return (
     <header className="navbar bg-success d-flex justify-content-start header">
       <h1 className="text-white">Calendar</h1>
-      <button className="btn btn-secondary mx-4" onClick={props.addEvent}>
+      <button className="btn btn-light mx-4" onClick={props.addEvent}>
         + Create
       </button>
     </header>
